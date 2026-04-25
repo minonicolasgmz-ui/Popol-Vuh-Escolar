7388c4e09cf5624a075e86cbbb4ad612ea691572
minonicolasgmz-ui <minonicolas.gmz@gmail.com>
2026-04-25 18:54:24 -0300

    cambio de base de datos

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,22 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-import path from 'path';
-
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const dbPath = path.join(process.cwd(), 'db', 'custom.db');
-
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
-    datasources: {
-      db: {
-        url: `file:${dbPath}`
-      }
-    }
+    log: process.env.NODE_ENV === 'development' ? ['query'] : [],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
